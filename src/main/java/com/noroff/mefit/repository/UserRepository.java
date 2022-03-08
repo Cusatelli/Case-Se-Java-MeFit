6e092cc9476cbe05eb58a2e8176fcd093b85154c
OmarAbdiAli <omarabdi.ali@se.experis.com>
2022-03-08 12:21:01 +0100

    feat: change to Long instead of Integer

--- src/main/java/com/noroff/mefit/repository/UserRepository.java
+++ src/main/java/com/noroff/mefit/repository/UserRepository.java
@@ -3,5 +3,5 @@ package com.noroff.mefit.repository;
 import com.noroff.mefit.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
