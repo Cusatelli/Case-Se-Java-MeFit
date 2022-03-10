2824c3eee496ff18d2a63c7d4f1e5cebc2ecee49
OmarAbdiAli <omarabdi.ali@se.experis.com>
2022-03-10 11:42:08 +0100

    fix: create service with crud methods

---
+++ src/main/java/com/noroff/mefit/service/SetService.java
@@ -0,0 +1,52 @@
+package com.noroff.mefit.service;
+
+import com.noroff.mefit.model.Exercise;
+import com.noroff.mefit.model.Set;
+import com.noroff.mefit.repository.SetRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public record SetService(SetRepository setRepository) {
+    public List<Set> getAll() {
+        return setRepository.findAll();
+    }
+
+    public Set create(Set set) {
+        return setRepository.save(set);
+    }
+
+    public Set getById(Long setId) {
+        if (!setRepository.existsById((setId))) {
+            return null;
+        }
+
+        return setRepository.findById(setId).orElse(null);
+    }
+
+    public Set update(Set set, Long setId) {
+        if (!setRepository.existsById((setId))) {
+            return null;
+        }
+
+        Set prevSet = setRepository.findById(setId).orElse(null);
+        if(prevSet == null) {
+            return null;
+        }
+
+        set.setId(prevSet.getId());
+        //change exercise id as well
+        set.setExercise(prevSet.getExercise());
+        return setRepository.save(set);
+    }
+
+    public Boolean delete(Long setId) {
+        if (!setRepository.existsById(setId)) {
+            return false;
+        }
+
+        setRepository.deleteById(setId);
+        return !setRepository.existsById(setId);
+    }
+}
