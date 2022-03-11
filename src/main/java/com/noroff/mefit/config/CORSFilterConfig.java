5a3aa8d672d216634c58bbef5a65e3a2530120a6
Cusatelli <einarvandevelde@gmail.com>
2022-03-11 09:32:17 +0100

    feat(CORS): allow CORS HttpsRequests

---
+++ src/main/java/com/noroff/mefit/config/CORSFilterConfig.java
@@ -0,0 +1,30 @@
+package com.noroff.mefit.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static com.noroff.mefit.config.ConfigSettings.*;
+
+/**
+ * Filter for enabling CORS support.
+ */
+@Component
+public class CORSFilterConfig extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response, final FilterChain filterChain) throws ServletException, IOException {
+        response.addHeader("Access-Control-Allow-Origin", ALLOW_ORIGIN);
+        response.addHeader("Access-Control-Allow-Methods", ALLOWED_METHODS);
+        response.addHeader("Access-Control-Allow-Headers", ALLOWED_HEADERS);
+        response.addHeader("Access-Control-Expose-Headers", EXPOSED_HEADERS);
+        response.addHeader("Access-Control-Allow-Credentials", ALLOW_CREDENTIALS.toString().toLowerCase());
+        response.addIntHeader("Access-Control-Max-Age", MAX_AGE);
+        filterChain.doFilter(request, response);
+    }
+}
