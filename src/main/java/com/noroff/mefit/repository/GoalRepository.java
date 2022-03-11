b41141f32393c7ab9799086bcf1ac6757d00ec6e
OmarAbdiAli <omarabdi.ali@se.experis.com>
2022-03-11 15:06:46 +0100

    fix: create goal repository

---
+++ src/main/java/com/noroff/mefit/repository/GoalRepository.java
@@ -0,0 +1,9 @@
+package com.noroff.mefit.repository;
+
+import com.noroff.mefit.model.Goal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GoalRepository extends JpaRepository<Goal, Long> {
+}
