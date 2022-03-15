1bb257f7b1049fdf35fdebf13097f8dde6762ed1
OmarAbdiAli <omarabdi.ali@se.experis.com>
2022-03-15 11:29:27 +0100

    fix: add api goalworkout in controller

---
+++ src/main/java/com/noroff/mefit/data/controller/GoalWorkoutController.java
@@ -0,0 +1,38 @@
+package com.noroff.mefit.data.controller;
+
+import com.noroff.mefit.data.model.GoalWorkout;
+import com.noroff.mefit.data.service.GoalWorkoutService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@Tag(name = "GoalWorkout")
+@RequestMapping("/api/goalWorkout")
+public record GoalWorkoutController(GoalWorkoutService goalWorkoutService) {
+    @GetMapping
+    public List<GoalWorkout> getAllGoals() {
+        return goalWorkoutService.getAll();
+    }
+
+    @PostMapping
+    public GoalWorkout create(@RequestBody GoalWorkout goalWorkout) {
+        return goalWorkoutService.create(goalWorkout);
+    }
+
+    @GetMapping("/{goalWorkoutId}")
+    public GoalWorkout getGoalById(@PathVariable Long goalWorkoutId) {
+        return goalWorkoutService.getById(goalWorkoutId);
+    }
+
+    @PatchMapping("/{goalWorkoutId}")
+    public GoalWorkout updateGoal(@RequestBody GoalWorkout goalWorkout, @PathVariable Long goalWorkoutId) {
+        return goalWorkoutService.update(goalWorkout, goalWorkoutId);
+    }
+
+    @DeleteMapping("/{goalWorkoutId}")
+    public Boolean deleteGoal(@PathVariable Long goalWorkoutId) {
+        return goalWorkoutService.delete(goalWorkoutId);
+    }
+}
