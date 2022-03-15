fd6f64466988200df77a50bc64e36a34004a0c24
OmarAbdiAli <omarabdi.ali@se.experis.com>
2022-03-15 11:29:00 +0100

    fix: add entity goalworkout in model

---
+++ src/main/java/com/noroff/mefit/data/model/GoalWorkout.java
@@ -0,0 +1,37 @@
+package com.noroff.mefit.data.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "goalworkout", schema = "public")
+public class GoalWorkout {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Column
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    private Timestamp end_date;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "workout_id")
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private Workout workoutId;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "goal_id")
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private Goal goalId;
+}
