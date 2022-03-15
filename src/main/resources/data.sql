ed5eb61cbf761bfc62ca0b9639183cb3820078f4
OmarAbdiAli <omarabdi.ali@se.experis.com>
2022-03-15 11:28:27 +0100

    feat: add insert values to GoalWorkout

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -130,6 +130,13 @@ VALUES (
            2
        );
 
+INSERT INTO public.goalworkout (end_date, workout_id, goal_id)
+VALUES (
+           '2022-03-20T12:45:00.000+00:00',
+            1,
+            2
+       );
+
 INSERT INTO public.program (name, category)
 VALUES (
            'Dumbbell',
