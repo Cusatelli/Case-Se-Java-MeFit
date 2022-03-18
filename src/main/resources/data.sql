b677a9900fe11f2b461534457449a900d361c94f
Gustav <meckan@hotmail.se>
2022-03-18 13:45:52 +0100

    fix: updated data.sql and schema.sql to use String for User entity

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,188 +1,148 @@
 INSERT INTO public.exercise (description, image, name, target_muscle_group, vid_link)
-VALUES (
-           'Squats are a functional exercise that benefit your joint and muscle health, as well as your posture',
-           'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/rw-april-jess-squat-1589233072.jpg?crop=0.667xw:1.00xh;0.148xw,0&resize=980:*',
-           'Squats',
-           'Glutes,Hamstrings,Quadriceps,Adductors,Calves,Core',
-           'https://youtu.be/aclHkVaku9U'
-       );
+VALUES ('Squats are a functional exercise that benefit your joint and muscle health, as well as your posture',
+        'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/rw-april-jess-squat-1589233072.jpg?crop=0.667xw:1.00xh;0.148xw,0&resize=980:*',
+        'Squats',
+        'Glutes,Hamstrings,Quadriceps,Adductors,Calves,Core',
+        'https://youtu.be/aclHkVaku9U');
 
 INSERT INTO public.exercise (description, image, name, target_muscle_group, vid_link)
-VALUES (
-           'The abdominal muscles used to hold the body rigid during the push-up are the rectus abdominis and the internal and external obliques.',
-           'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic2.therichestimages.com%2Fwordpress%2Fwp-content%2Fuploads%2F2019%2F02%2FPush-Up.jpg&f=1&nofb=1',
-           'Push-Up',
-           'Chest,Shoulders,Core,Back,Legs,Arms',
-           'https://www.youtube.com/watch?v=eFOSh8vpd6I'
-       );
+VALUES ('The abdominal muscles used to hold the body rigid during the push-up are the rectus abdominis and the internal and external obliques.',
+        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic2.therichestimages.com%2Fwordpress%2Fwp-content%2Fuploads%2F2019%2F02%2FPush-Up.jpg&f=1&nofb=1',
+        'Push-Up',
+        'Chest,Shoulders,Core,Back,Legs,Arms',
+        'https://www.youtube.com/watch?v=eFOSh8vpd6I');
 
 INSERT INTO public.exercise (description, image, name, target_muscle_group, vid_link)
-VALUES (
-           'Hold a dumbbell with one hand along the side of your body.',
-           'https://dumbbell-exercises.com/wp-content/uploads/2016/12/3.gif',
-           'Dumbbell Side Bend',
-           'Core',
-           'https://www.youtube.com/watch?v=GXM-iYXhQfY'
-       );
+VALUES ('Hold a dumbbell with one hand along the side of your body.',
+        'https://dumbbell-exercises.com/wp-content/uploads/2016/12/3.gif',
+        'Dumbbell Side Bend',
+        'Core',
+        'https://www.youtube.com/watch?v=GXM-iYXhQfY');
 
-INSERT INTO public.user (admin, contributor, first_name, last_name, password)
-VALUES (
-           FALSE,
-           FALSE,
-           'John', 'Doe',
-           '123WeakPassword'
-       );
+INSERT INTO public.user (id, admin, contributor, first_name, last_name, password)
+VALUES ('Alice',
+        FALSE,
+        FALSE,
+        'John', 'Doe',
+        '123WeakPassword');
 
-INSERT INTO public.user (admin, contributor, first_name, last_name, password)
-VALUES (
-           TRUE,
-           TRUE,
-           'Erica', 'Jones',
-           'Ball199x=fun'
-       );
+INSERT INTO public.user (id, admin, contributor, first_name, last_name, password)
+VALUES ('Bob',
+        TRUE,
+        TRUE,
+        'Erica', 'Jones',
+        'Ball199x=fun');
 
-INSERT INTO public.user (admin, contributor, first_name, last_name, password)
-VALUES (
-           FALSE,
-           TRUE,
-           'Silver', 'Adamson',
-           '092OkEs#!2fKfESz=67'
-       );
+INSERT INTO public.user (id, admin, contributor, first_name, last_name, password)
+VALUES ('Mal',
+        FALSE,
+        TRUE,
+        'Silver', 'Adamson',
+        '092OkEs#!2fKfESz=67');
 
-INSERT INTO public.user (admin, contributor, first_name, last_name, password)
-VALUES (
-           FALSE,
-           FALSE,
-           'Alice', 'Shoemaker',
-           'Sole42=Foot42!#Leather'
-       );
+INSERT INTO public.user (id, admin, contributor, first_name, last_name, password)
+VALUES ('Foo',
+        FALSE,
+        FALSE,
+        'Alice', 'Shoemaker',
+        'Sole42=Foot42!#Leather');
 
 INSERT INTO public.workout (complete, name, type, set_id)
-VALUES (
-           FALSE,
-           'Abdominal',
-           'Agility',
-            1
-       );
+VALUES (FALSE,
+        'Abdominal',
+        'Agility',
+        1);
 
 INSERT INTO public.workout (complete, name, type, set_id)
-VALUES (
-           TRUE,
-           'Bodybuilding',
-           'Strength',
-            1
-       );
+VALUES (TRUE,
+        'Bodybuilding',
+        'Strength',
+        1);
 
 INSERT INTO public.workout (complete, name, type, set_id)
-VALUES (
-           FALSE,
-           'Legs',
-           'Strength',
-            1
-       );
+VALUES (FALSE,
+        'Legs',
+        'Strength',
+        1);
 
 INSERT INTO public.address (address_line_1, address_line_2, address_line_3, city, country, postal_code)
-VALUES (
-           'Tongass Ave',
-           '2417',
-           '#111',
-           'Ketchikan',
-           'USA',
-           '99901'
-       );
+VALUES ('Tongass Ave',
+        '2417',
+        '#111',
+        'Ketchikan',
+        'USA',
+        '99901');
 
 INSERT INTO public.address (address_line_1, address_line_2, address_line_3, city, country, postal_code)
-VALUES (
-           'Killington Rd',
-           '4763',
-           null,
-           'Killington',
-           'USA',
-           '05751'
-       );
+VALUES ('Killington Rd',
+        '4763',
+        null,
+        'Killington',
+        'USA',
+        '05751');
 
-INSERT INTO public.profile (set_id, user_id, goal_id, address_id, program_id, workout_id, disabilities, height, medical_conditions, weight)
-VALUES (
-           1,
-           1,
-           1,
-           1,
-           1,
-           1,
-           '9 fingers, blurred vision',
-           192,
-           null,
-           94
-       );
+INSERT INTO public.profile (set_id, user_id, goal_id, address_id, program_id, workout_id, disabilities, height,
+                            medical_conditions, weight)
+VALUES (1,
+        'Alice',
+        1,
+        1,
+        1,
+        1,
+        '9 fingers, blurred vision',
+        192,
+        null,
+        94);
 
-INSERT INTO public.profile (set_id, user_id, goal_id, address_id, program_id, workout_id, disabilities, height, medical_conditions, weight)
-VALUES (
-           2,
-           1,
-           1,
-           1,
-           1,
-           1,
-           null,
-           179,
-           null,
-           87
-       );
+INSERT INTO public.profile (set_id, user_id, goal_id, address_id, program_id, workout_id, disabilities, height,
+                            medical_conditions, weight)
+VALUES (2,
+        'Bob',
+        1,
+        1,
+        1,
+        1,
+        null,
+        179,
+        null,
+        87);
 
 INSERT INTO public.set (exercise_repetition, exercise_id)
-VALUES (
-           4,
-           1
-       );
+VALUES (4,
+        1);
 
 INSERT INTO public.set (exercise_repetition, exercise_id)
-VALUES (
-           5,
-           2
-       );
+VALUES (5,
+        2);
 
 INSERT INTO public.program (name, category)
-VALUES (
-           'Dumbbell',
-           'Muscle building'
-       );
+VALUES ('Dumbbell',
+        'Muscle building');
 
 INSERT INTO public.goal (achieved, end_date, program_id)
-VALUES (
-            TRUE,
-            '2022-03-20T12:45:00.000+00:00',
-            1
-       );
+VALUES (TRUE,
+        '2022-03-20T12:45:00.000+00:00',
+        1);
 
 INSERT INTO public.goal (achieved, end_date, program_id)
-VALUES (
-           FALSE,
-           now(),
-            1
-       );
+VALUES (FALSE,
+        now(),
+        1);
 
 INSERT INTO public.program_workout(workout_id, program_id)
-VALUES (
-           3,
-           1
-       );
+VALUES (3,
+        1);
 
 INSERT INTO public.program_workout(workout_id, program_id)
-VALUES (
-           1,
-           1
-       );
+VALUES (1,
+        1);
 
 INSERT INTO public.goal_workout(end_date, workout_id, goal_id)
-VALUES (
-           '2022-03-20T12:45:00.000+00:00',
-           1,
-           2
-       );
+VALUES ('2022-03-20T12:45:00.000+00:00',
+        1,
+        2);
 
 INSERT INTO public.goal_workout(end_date, workout_id, goal_id)
-VALUES (
-           now(),
-           2,
-           1
-       );
+VALUES (now(),
+        2,
+        1);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,94 +1,105 @@
 DROP TABLE IF EXISTS public.exercise, public.user, public.workout, public.address, public.profile, public.set, public.program, public.goal, public.program_workout, public.goal_workout CASCADE;
 
 
-CREATE TABLE public.exercise (
-    id BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY,
-    description VARCHAR(200) NOT NULL DEFAULT (''),
-    image VARCHAR(255),
-    name VARCHAR(50) NOT NULL,
+CREATE TABLE public.exercise
+(
+    id                  BIGINT       NOT NULL GENERATED ALWAYS AS IDENTITY,
+    description         VARCHAR(200) NOT NULL DEFAULT (''),
+    image               VARCHAR(255),
+    name                VARCHAR(50)  NOT NULL,
     target_muscle_group VARCHAR(255),
-    vid_link VARCHAR(255),
+    vid_link            VARCHAR(255),
     PRIMARY KEY (id)
 );
 
-CREATE TABLE public.user (
-    id BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY,
-    admin BOOLEAN NOT NULL DEFAULT FALSE,
-    contributor BOOLEAN NOT NULL DEFAULT FALSE,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50),
-    password VARCHAR(50) NOT NULL,
+CREATE TABLE public.user
+(
+--     id BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY,
+    id          VARCHAR     NOT NULL ,
+    admin       BOOLEAN     NOT NULL DEFAULT FALSE,
+    contributor BOOLEAN     NOT NULL DEFAULT FALSE,
+    first_name  VARCHAR(50) NOT NULL,
+    last_name   VARCHAR(50),
+    password    VARCHAR(50) NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE public.workout (
-    id BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY,
-    complete BOOLEAN NOT NULL DEFAULT FALSE,
-    name VARCHAR(50) NOT NULL,
-    type VARCHAR NOT NULL,
-    set_id BIGINT,
+CREATE TABLE public.workout
+(
+    id       BIGINT      NOT NULL GENERATED ALWAYS AS IDENTITY,
+    complete BOOLEAN     NOT NULL DEFAULT FALSE,
+    name     VARCHAR(50) NOT NULL,
+    type     VARCHAR     NOT NULL,
+    set_id   BIGINT,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE public.address (
-    id BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY,
-    address_line_1 VARCHAR NOT NULL,
+CREATE TABLE public.address
+(
+    id             BIGINT      NOT NULL GENERATED ALWAYS AS IDENTITY,
+    address_line_1 VARCHAR     NOT NULL,
     address_line_2 VARCHAR,
     address_line_3 VARCHAR,
-    city VARCHAR(70) NOT NULL,
-    country VARCHAR(70) NOT NULL,
-    postal_code VARCHAR(12) NOT NULL,
+    city           VARCHAR(70) NOT NULL,
+    country        VARCHAR(70) NOT NULL,
+    postal_code    VARCHAR(12) NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE public.profile (
-    id BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY,
-    disabilities VARCHAR,
-    height INTEGER NOT NULL,
+CREATE TABLE public.profile
+(
+    id                 BIGINT  NOT NULL GENERATED ALWAYS AS IDENTITY,
+    disabilities       VARCHAR,
+    height             INTEGER,
     medical_conditions VARCHAR,
-    weight INTEGER NOT NULL,
-    set_id BIGINT,
-    user_id BIGINT,
-    goal_id BIGINT,
-    address_id BIGINT,
-    program_id BIGINT,
-    workout_id BIGINT,
+    weight             INTEGER,
+    set_id             BIGINT,
+    user_id            VARCHAR,
+    goal_id            BIGINT,
+    address_id         BIGINT,
+    program_id         BIGINT,
+    workout_id         BIGINT,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE public.set (
-    id BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY,
+CREATE TABLE public.set
+(
+    id                  BIGINT  NOT NULL GENERATED ALWAYS AS IDENTITY,
     exercise_repetition INTEGER NOT NULL,
-    exercise_id BIGINT,
+    exercise_id         BIGINT,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE public.program (
-     id BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY,
-     name VARCHAR(50) NOT NULL,
-     category VARCHAR(100),
-     PRIMARY KEY (id)
+CREATE TABLE public.program
+(
+    id       BIGINT      NOT NULL GENERATED ALWAYS AS IDENTITY,
+    name     VARCHAR(50) NOT NULL,
+    category VARCHAR(100),
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE public.goal (
-    id BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY,
-    end_date TIMESTAMP DEFAULT now(),
-    achieved BOOLEAN NOT NULL,
+CREATE TABLE public.goal
+(
+    id         BIGINT  NOT NULL GENERATED ALWAYS AS IDENTITY,
+    end_date   TIMESTAMP DEFAULT now(),
+    achieved   BOOLEAN NOT NULL,
     program_id BIGINT,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE public.program_workout (
-                            id BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY,
-                            workout_id BIGINT,
-                            program_id BIGINT,
-                            PRIMARY KEY (id)
-  );
-
-CREATE TABLE public.goal_workout (
-    id BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY,
-    end_date timestamp NOT NULL,
+CREATE TABLE public.program_workout
+(
+    id         BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY,
     workout_id BIGINT,
-    goal_id BIGINT,
+    program_id BIGINT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE public.goal_workout
+(
+    id         BIGINT    NOT NULL GENERATED ALWAYS AS IDENTITY,
+    end_date   timestamp NOT NULL,
+    workout_id BIGINT,
+    goal_id    BIGINT,
     PRIMARY KEY (id)
 );
