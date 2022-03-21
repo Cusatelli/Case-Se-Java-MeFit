ea36d098708ec65d458863cee86a72a5a3e059d7
Cusatelli <einarvandevelde@gmail.com>
2022-03-21 19:04:38 +0100

    style: minor reformatting

--- src/main/java/com/noroff/mefit/data/repository/ProgramRepository.java
+++ src/main/java/com/noroff/mefit/data/repository/ProgramRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProgramRepository extends JpaRepository<Program, Long> {
-}
+public interface ProgramRepository extends JpaRepository<Program, Long> { }
