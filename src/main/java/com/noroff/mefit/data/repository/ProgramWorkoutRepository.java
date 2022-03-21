5093f379874949eaef70447921dcf9d0f95e6ad8
Cusatelli <einarvandevelde@gmail.com>
2022-03-21 13:48:08 +0100

    refactor: remove redundant ProgramWorkout

--- src/main/java/com/noroff/mefit/data/repository/ProgramWorkoutRepository.java
+++
@@ -1,9 +0,0 @@
-package com.noroff.mefit.data.repository;
-
-import com.noroff.mefit.data.model.ProgramWorkout;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProgramWorkoutRepository extends JpaRepository<ProgramWorkout, Long> {
-}
